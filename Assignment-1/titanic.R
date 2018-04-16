4a0898198dfd30987eddec9aee53e80bf5a2ede3
izakdekom <izakdekom@gmail.com>
2018-04-16 17:24:33 +0200

    Improved train/test split method, now using createDataPartition() from the caret package to retain class balance

--- Assignment-1/titanic.R
+++ Assignment-1/titanic.R
@@ -13,18 +13,10 @@ data = na.exclude(data)
 data = data[-c(1,4,9,11)]
 data$Survived = as.factor(data$Survived)
 
-# split for train/test data 0.7/0.3 to compare classifiers later
-set.seed(100)
-nall = nrow(data)
-ntrain = floor(0.7 * nall)
-ntest = floor(0.3* nall)
-index = seq(1:nall)
-trainIndex = sample(index, ntrain) 
-testIndex = index[-trainIndex]
-
-# make train and test sets
-train = data[trainIndex,]
-test = data[testIndex,]
+# split for train/test data
+trainIndex = createDataPartition(data$Survived, p = .7, list = FALSE)
+train = data[ trainIndex,]
+test  = data[-trainIndex,]
 
 # caret machine learning control: number of cross validation folds and repeats
 control = trainControl(method="repeatedcv", number=10, repeats=2)
@@ -37,5 +29,3 @@ gbm_result = confusionMatrix(predict(gbm,test),test$Survived)
 # ranfom forest model: https://en.wikipedia.org/wiki/Random_forest
 rf = train(train[-1],train[,1], method = "rf", trControl = control)
 rf_result = confusionMatrix(predict(rf,test),test$Survived)
-
-
