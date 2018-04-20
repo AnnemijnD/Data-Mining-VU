fa8aa645e0873041831e09ecfce3249751c14b4a
Konrad Karaś <knrdkrs@gmail.com>
2018-04-20 11:26:00 +0200

    task implementation

---
+++ Assignment-1/2. Titanic/kokar_titanic.R
@@ -0,0 +1,100 @@
+train <- read.csv("train.csv")
+test <- read.csv("test.csv")
+library(caret)
+# Install and load required packages for decision trees and forests
+library(rpart)
+# install.packages('randomForest')
+library(randomForest)
+# install.packages('party')
+library(party)
+
+library(doMC)
+# parallel processing: set number of cores
+registerDoMC(cores = 4)
+
+# Join together the test and train sets for easier feature engineering
+test$Survived <- NA
+combi <- rbind(train, test)
+
+# Convert to a string
+combi$Name <- as.character(combi$Name)
+
+# Engineered variable: Title
+combi$Title <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
+combi$Title <- sub(' ', '', combi$Title)
+# Combine small title groups
+combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
+# Convert to a factor
+combi$Title <- factor(combi$Title)
+
+# Engineered variable: Family size
+combi$FamilySize <- combi$SibSp + combi$Parch + 1
+
+# Engineered variable: Family
+combi$Surname <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][1]})
+combi$FamilyID <- paste(as.character(combi$FamilySize), combi$Surname, sep="")
+combi$FamilyID[combi$FamilySize <= 2] <- 'Small'
+# Delete erroneous family IDs
+famIDs <- data.frame(table(combi$FamilyID))
+famIDs <- famIDs[famIDs$Freq <= 2,]
+combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
+# Convert to a factor
+combi$FamilyID <- factor(combi$FamilyID)
+
+# Fill in Age NAs
+summary(combi$Age)
+Agefit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize, 
+                data=combi[!is.na(combi$Age),], method="anova")
+combi$Age[is.na(combi$Age)] <- predict(Agefit, combi[is.na(combi$Age),])
+# Check what else might be missing
+summary(combi)
+# Fill in Embarked blanks
+summary(combi$Embarked)
+which(combi$Embarked == '')
+combi$Embarked[c(62,830)] = "S"
+combi$Embarked <- factor(combi$Embarked)
+# Fill in Fare NAs
+summary(combi$Fare)
+which(is.na(combi$Fare))
+combi$Fare[1044] <- median(combi$Fare, na.rm=TRUE)
+
+# Set deck by cabin
+combi$Cabin <- as.character(combi$Cabin)
+combi$Deck <- sapply(combi$Cabin, FUN=function(x) {
+    if(x == '') {
+      'U'
+    } else {
+      substring(x, 1, 1)
+    }
+})
+combi$Deck <- factor(combi$Deck)
+
+# Set ticket types
+combi$Ticket <- as.character(combi$Ticket)
+combi$TicketType <- sapply(combi$Ticket, FUN=function(x) {
+  temp = strsplit(x, split=' ')[[1]][1]
+  if(is.na(as.numeric(temp))) {
+    temp
+  } else {
+    'num'
+  }
+})
+combi$TicketType <- factor(combi$TicketType)
+
+# Split back into test and train sets
+train <- combi[1:891,]
+test <- combi[892:1309,]
+
+# Build condition inference tree Random Forest
+set.seed(415)
+fit <- cforest(as.factor(Survived) ~ Pclass + TicketType + Sex + Deck + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+               data = train, controls=cforest_unbiased(ntree=2000, mtry=3)) 
+
+# RESULT
+Prediction <- predict(fit, test, OOB=TRUE, type = "response")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(submit, file = "ciforest.csv", row.names = FALSE)
+
+
