fdfc1f2e81f012fa75a7944f2b0862f3abeb3f91
tanjina.islam <t.islam@student.vu.nl>
2018-05-17 12:50:15 +0200

    replaced NA values with 0 for comp_rate and for rest of others with -1

--- Assignment-2/data-exploration-sample_10-micky.R
+++ Assignment-2/data-exploration-sample_10-micky.R
@@ -26,8 +26,32 @@ data_sample <- data_sample[-c(1)]
 
 attach(data_sample)
 
-# filling up missing values with -1
-data_sample[1:54] = as.data.frame(lapply(data_sample[1:54], function(x) ifelse(is.na(x), -1, x)))
+
+# filling up missing values with 0 for comp_rate 1 - comp_rate 8
+
+data_sample[28] = as.data.frame(lapply(data_sample[28], function(x) ifelse(is.na(x), 0, x)))
+data_sample[31] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[34] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[37] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[40] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[43] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[46] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+data_sample[49] = as.data.frame(lapply(data_sample[31], function(x) ifelse(is.na(x), 0, x)))
+
+# filling up missing values with -1 for rest of the other attributes
+data_sample[1:27] = as.data.frame(lapply(data_sample[1:27], function(x) ifelse(is.na(x), -1, x)))
+data_sample[29:30] = as.data.frame(lapply(data_sample[29:30], function(x) ifelse(is.na(x), -1, x)))
+data_sample[32:33] = as.data.frame(lapply(data_sample[32:33], function(x) ifelse(is.na(x), -1, x)))
+data_sample[35:36] = as.data.frame(lapply(data_sample[35:36], function(x) ifelse(is.na(x), -1, x)))
+data_sample[38:39] = as.data.frame(lapply(data_sample[38:39], function(x) ifelse(is.na(x), -1, x)))
+data_sample[41:42] = as.data.frame(lapply(data_sample[41:42], function(x) ifelse(is.na(x), -1, x)))
+data_sample[44:45] = as.data.frame(lapply(data_sample[44:45], function(x) ifelse(is.na(x), -1, x)))
+data_sample[47:48] = as.data.frame(lapply(data_sample[47:48], function(x) ifelse(is.na(x), -1, x)))
+data_sample[50:54] = as.data.frame(lapply(data_sample[50:54], function(x) ifelse(is.na(x), -1, x)))
 
 # Write CSV in R
 write.csv(data_sample, file = "sample_train_10prc_no_missing_val.csv")
+
+data_new = read.csv("sample_train_10prc_no_missing_val.csv")
+data_new
+
