fc83bf0551dbe452de9784bd11594c255f045368
Konrad Karaś <knrdkrs@gmail.com>
2018-05-18 22:51:12 +0200

    train code

--- Assignment-2/train.R
+++ Assignment-2/train.R
@@ -1,48 +1,46 @@
 data_raw = read.csv('sample_train_10prc_no_missing_val.csv')
 
-data_raw$site_id <- as.character(data_raw$site_id)
-data_raw$visitor_location_country_id <- as.character(data_raw$visitor_location_country_id)
-data_raw$prop_country_id <- as.character(data_raw$prop_country_id)
-data_raw$prop_starrating <- as.character(data_raw$prop_starrating)
-data_raw$prop_brand_bool <- as.character(data_raw$prop_brand_bool)
-data_raw$promotion_flag <- as.character(data_raw$promotion_flag)
-data_raw$srch_destination_id <- as.character(data_raw$srch_destination_id)
-data_raw$srch_length_of_stay <- as.character(data_raw$srch_length_of_stay)
-data_raw$srch_adults_count <- as.character(data_raw$srch_adults_count)
-data_raw$srch_children_count <- as.character(data_raw$srch_children_count)
-data_raw$srch_room_count <- as.character(data_raw$srch_room_count)
-data_raw$srch_saturday_night_bool <- as.character(data_raw$srch_saturday_night_bool)
-data_raw$random_bool <- as.character(data_raw$random_bool)
-data_raw$comp1_rate <- as.character(data_raw$comp1_rate)
-data_raw$comp1_inv <- as.character(data_raw$comp1_inv)
-data_raw$comp2_rate <- as.character(data_raw$comp2_rate)
-data_raw$comp2_inv <- as.character(data_raw$comp2_inv)
-data_raw$comp3_rate <- as.character(data_raw$comp3_rate)
-data_raw$comp3_inv <- as.character(data_raw$comp3_inv)
-data_raw$comp4_rate <- as.character(data_raw$comp4_rate)
-data_raw$comp4_inv <- as.character(data_raw$comp4_inv)
-data_raw$comp5_rate <- as.character(data_raw$comp5_rate)
-data_raw$comp5_inv <- as.character(data_raw$comp5_inv)
-data_raw$comp6_rate <- as.character(data_raw$comp6_rate)
-data_raw$comp6_inv <- as.character(data_raw$comp6_inv)
-data_raw$comp7_rate <- as.character(data_raw$comp7_rate)
-data_raw$comp7_inv <- as.character(data_raw$comp7_inv)
-data_raw$comp8_rate <- as.character(data_raw$comp8_rate)
-data_raw$comp8_inv <- as.character(data_raw$comp8_inv)
-data_raw$click_bool <- as.character(data_raw$click_bool)
-data_raw$gross_bookings_usd <- as.character(data_raw$gross_bookings_usd)
-data_raw$booking_bool <- as.character(data_raw$booking_bool)
+data_raw$site_id <- factor(make.names(data_raw$site_id))
+#data_raw$visitor_location_country_id <- factor(make.names(data_raw$visitor_location_country_id))
+#data_raw$prop_country_id <- factor(make.names(data_raw$prop_country_id))
+data_raw$prop_starrating <- factor(make.names(data_raw$prop_starrating))
+data_raw$prop_brand_bool <- factor(make.names(data_raw$prop_brand_bool))
+data_raw$promotion_flag <- factor(make.names(data_raw$promotion_flag))
+#data_raw$srch_destination_id <- factor(make.names(data_raw$srch_destination_id))
+data_raw$srch_length_of_stay <- factor(make.names(data_raw$srch_length_of_stay))
+data_raw$srch_adults_count <- factor(make.names(data_raw$srch_adults_count))
+data_raw$srch_children_count <- factor(make.names(data_raw$srch_children_count))
+data_raw$srch_room_count <- factor(make.names(data_raw$srch_room_count))
+data_raw$srch_saturday_night_bool <- factor(make.names(data_raw$srch_saturday_night_bool))
+data_raw$random_bool <- factor(make.names(data_raw$random_bool))
+data_raw$comp1_rate <- factor(make.names(data_raw$comp1_rate))
+data_raw$comp1_inv <- factor(make.names(data_raw$comp1_inv))
+data_raw$comp2_rate <- factor(make.names(data_raw$comp2_rate))
+data_raw$comp2_inv <- factor(make.names(data_raw$comp2_inv))
+data_raw$comp3_rate <- factor(make.names(data_raw$comp3_rate))
+data_raw$comp3_inv <- factor(make.names(data_raw$comp3_inv))
+data_raw$comp4_rate <- factor(make.names(data_raw$comp4_rate))
+data_raw$comp4_inv <- factor(make.names(data_raw$comp4_inv))
+data_raw$comp5_rate <- factor(make.names(data_raw$comp5_rate))
+data_raw$comp5_inv <- factor(make.names(data_raw$comp5_inv))
+data_raw$comp6_rate <- factor(make.names(data_raw$comp6_rate))
+data_raw$comp6_inv <- factor(make.names(data_raw$comp6_inv))
+data_raw$comp7_rate <- factor(make.names(data_raw$comp7_rate))
+data_raw$comp7_inv <- factor(make.names(data_raw$comp7_inv))
+data_raw$comp8_rate <- factor(make.names(data_raw$comp8_rate))
+data_raw$comp8_inv <- factor(make.names(data_raw$comp8_inv))
+data_raw$click_bool <- factor(make.names(data_raw$click_bool))
+data_raw$booking_bool <- factor(make.names(data_raw$booking_bool))
 
-data = data_raw[1:1000,]
+data = data_raw[1:10000,]
 
 library('caret')
 set.seed(1)
 
 
 #Spliting training set into two parts based on outcome: 75% and 25%
-index <- createDataPartition(data$booking_bool, p=0.75, list=FALSE)
-trainSet <- data[ index,]
-testSet <- data[-index,]
+trainSet <- data[1:7500,]
+testSet <- data[7501:10000,]
 
 #Defining the training controls for multiple models
 fitControl <- trainControl(
@@ -52,11 +50,17 @@ fitControl <- trainControl(
   classProbs = T,
   verboseIter = TRUE)
 
-predictors <- colnames(data)[2:52]
+predictors <- c("prop_location_score2","date_time","srch_destination_id","price_usd","srch_booking_window","prop_log_historical_price","site_id")
 outcomeName <- 'booking_bool'
 
 model_rf<-train(trainSet[,predictors],trainSet[,outcomeName],method='rf',trControl=fitControl,tuneLength=3)
 
+importance <- varImp(model_rf, scale=FALSE)
+plot(importance)
 
 testSet$pred_rf<-predict(object = model_rf,testSet[,predictors])
 confusionMatrix(testSet$booking_bool,testSet$pred_rf)
+
+
+library('FSelector')
+res <- gain.ratio(g~., data)
