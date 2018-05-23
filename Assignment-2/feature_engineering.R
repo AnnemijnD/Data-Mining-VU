3769434fae94b3be239ba2e397fb4793d40a7f48
tanjina.islam <t.islam@student.vu.nl>
2018-05-23 17:04:35 +0200

    more feature engineering formulas added

--- Assignment-2/feature_engineering.R
+++ Assignment-2/feature_engineering.R
@@ -2,6 +2,8 @@
 data = read.csv('data_sample/sample_train_10prc_no_missing_val.csv')
 attach(data)
 
+data <- data[-c(1)]
+
 # 01 : extract hour, day and motnh from date_time feature
 # date_time -> date_hour, date_day, date_month
 data$date_time <- as.character(data$date_time)
@@ -32,6 +34,32 @@ data["prop_star_rating_monotonic"] <- abs(prop_starrating - mean(prop_starrating
 # 06 : hotel rank in region
 data$srch_destination_id <- factor(data$srch_destination_id)
 
+# 07 : window_count = srch room count ??? max(srch booking window) + srch booking window.
+data["window_count"] = srch_room_count * max(srch_booking_window) + srch_booking_window 
+
+# 08 : adult_child_count = srch room count ??? max(srch booking window) + srch children count
+data["adult_child_count"] = srch_adults_count * max(srch_children_count) + srch_children_count 
+
+# 09 : hotel's historical price - current price
+data["ump"] = exp(prop_log_historical_price) - price_usd
+
+
+# 10 : fee per person
+data["fee_per_person"] = (price_usd * srch_room_count)/(srch_adults_count + srch_children_count)
+
+# 11 : total fee for a hotel
+data["total_fee"] = price_usd*srch_room_count
+
+# 12 :
+data["score2ma"] = prop_location_score2 * srch_query_affinity_score
+
+# 13 :
+data["score1d2"] = (prop_location_score2 + 0.0001)/(prop_location_score1 + 0.0001)
+
+
+
 
 # Write CSV in R
-write.csv(data_sample, file = "sample_train_10prc_feature_eng.csv")
+write.csv(data, file = "sample_train_10prc_feature_eng.csv")
+data_new = read.csv("sample_train_10prc_feature_eng.csv")
+data_new
