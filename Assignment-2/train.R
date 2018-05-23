e123d06f8684774e6119d0240de5b69acabffbd1
Konrad Karaś <knrdkrs@gmail.com>
2018-05-23 20:10:40 +0200

    training random forest

--- Assignment-2/train.R
+++ Assignment-2/train.R
@@ -1,77 +1,92 @@
-data_raw = read.csv('sample_train_10prc_no_missing_val.csv')
-
-
-# Feature Engineering: DateTime
-data_raw$date_time <- as.character(data_raw$date_time)
-data_raw$date_day <- sapply(data_raw$date_time, FUN=function(x) {strsplit(x, split='[- :]')[[1]][3]})
-data_raw$date_month <- sapply(data_raw$date_time, FUN=function(x) {strsplit(x, split='[- :]')[[1]][2]})
-data_raw$date_hour <- sapply(data_raw$date_time, FUN=function(x) {strsplit(x, split='[- :]')[[1]][4]})
-data_raw$date_day <- factor(make.names(data_raw$date_day))
-data_raw$date_month <- factor(make.names(data_raw$date_month))
-data_raw$date_hour <- factor(make.names(data_raw$date_hour))
-
-# Feature Engineering: 
+data = read.csv('sample_train_10prc_feature_eng.csv')
 
 # Prepare factors
-data_raw$site_id <- factor(make.names(data_raw$site_id))
-#data_raw$visitor_location_country_id <- factor(make.names(data_raw$visitor_location_country_id))
-#data_raw$prop_country_id <- factor(make.names(data_raw$prop_country_id))
-data_raw$prop_starrating <- factor(make.names(data_raw$prop_starrating))
-data_raw$prop_brand_bool <- factor(make.names(data_raw$prop_brand_bool))
-data_raw$promotion_flag <- factor(make.names(data_raw$promotion_flag))
-#data_raw$srch_destination_id <- factor(make.names(data_raw$srch_destination_id))
-data_raw$srch_length_of_stay <- factor(make.names(data_raw$srch_length_of_stay))
-data_raw$srch_adults_count <- factor(make.names(data_raw$srch_adults_count))
-data_raw$srch_children_count <- factor(make.names(data_raw$srch_children_count))
-data_raw$srch_room_count <- factor(make.names(data_raw$srch_room_count))
-data_raw$srch_saturday_night_bool <- factor(make.names(data_raw$srch_saturday_night_bool))
-data_raw$random_bool <- factor(make.names(data_raw$random_bool))
-data_raw$comp1_rate <- factor(make.names(data_raw$comp1_rate))
-data_raw$comp1_inv <- factor(make.names(data_raw$comp1_inv))
-data_raw$comp2_rate <- factor(make.names(data_raw$comp2_rate))
-data_raw$comp2_inv <- factor(make.names(data_raw$comp2_inv))
-data_raw$comp3_rate <- factor(make.names(data_raw$comp3_rate))
-data_raw$comp3_inv <- factor(make.names(data_raw$comp3_inv))
-data_raw$comp4_rate <- factor(make.names(data_raw$comp4_rate))
-data_raw$comp4_inv <- factor(make.names(data_raw$comp4_inv))
-data_raw$comp5_rate <- factor(make.names(data_raw$comp5_rate))
-data_raw$comp5_inv <- factor(make.names(data_raw$comp5_inv))
-data_raw$comp6_rate <- factor(make.names(data_raw$comp6_rate))
-data_raw$comp6_inv <- factor(make.names(data_raw$comp6_inv))
-data_raw$comp7_rate <- factor(make.names(data_raw$comp7_rate))
-data_raw$comp7_inv <- factor(make.names(data_raw$comp7_inv))
-data_raw$comp8_rate <- factor(make.names(data_raw$comp8_rate))
-data_raw$comp8_inv <- factor(make.names(data_raw$comp8_inv))
-data_raw$click_bool <- factor(make.names(data_raw$click_bool))
-data_raw$booking_bool <- factor(make.names(data_raw$booking_bool))
+data$site_id <- factor(make.names(data$site_id))
+#data$visitor_location_country_id <- factor(make.names(data$visitor_location_country_id))
+#data$prop_country_id <- factor(make.names(data$prop_country_id))
+#data$prop_starrating <- factor(make.names(data$prop_starrating))
+data$prop_brand_bool <- factor(make.names(data$prop_brand_bool))
+data$promotion_flag <- factor(make.names(data$promotion_flag))
+#data$srch_destination_id <- factor(make.names(data$srch_destination_id))
+#data$srch_length_of_stay <- factor(make.names(data$srch_length_of_stay))
+#data$srch_adults_count <- factor(make.names(data$srch_adults_count))
+#data$srch_children_count <- factor(make.names(data$srch_children_count))
+#data$srch_room_count <- factor(make.names(data$srch_room_count))
+data$srch_saturday_night_bool <- factor(make.names(data$srch_saturday_night_bool))
+data$random_bool <- factor(make.names(data$random_bool))
+data$comp1_rate <- factor(make.names(data$comp1_rate))
+data$comp1_inv <- factor(make.names(data$comp1_inv))
+data$comp2_rate <- factor(make.names(data$comp2_rate))
+data$comp2_inv <- factor(make.names(data$comp2_inv))
+data$comp3_rate <- factor(make.names(data$comp3_rate))
+data$comp3_inv <- factor(make.names(data$comp3_inv))
+data$comp4_rate <- factor(make.names(data$comp4_rate))
+data$comp4_inv <- factor(make.names(data$comp4_inv))
+data$comp5_rate <- factor(make.names(data$comp5_rate))
+data$comp5_inv <- factor(make.names(data$comp5_inv))
+data$comp6_rate <- factor(make.names(data$comp6_rate))
+data$comp6_inv <- factor(make.names(data$comp6_inv))
+data$comp7_rate <- factor(make.names(data$comp7_rate))
+data$comp7_inv <- factor(make.names(data$comp7_inv))
+data$comp8_rate <- factor(make.names(data$comp8_rate))
+data$comp8_inv <- factor(make.names(data$comp8_inv))
+data$click_bool <- factor(make.names(data$click_bool))
+data$booking_bool <- factor(make.names(data$booking_bool))
 
-data = data_raw[1:100000,]
+# engineered features
+data$date_day <- factor(make.names(data$date_day))
+data$date_month <- factor(make.names(data$date_month))
+data$date_hour <- factor(make.names(data$date_hour))
+data$children_flag <- factor(make.names(data$children_flag))
+
+
+# TRAIN
 
 library('caret')
 set.seed(1)
 
 
 #Spliting training set into two parts based on outcome: 75% and 25%
-trainSet <- data[1:7500,]
-testSet <- data[7501:10000,]
+index <- createDataPartition(data$booking_bool, p=0.75, list=FALSE)
+trainSet <- data[ index,]
+testSet <- data[-index,]
 
 #Defining the training controls for multiple models
 fitControl <- trainControl(
   method = "cv",
-  number = 5,
+  number = 1,
   savePredictions = 'final',
   classProbs = T,
   verboseIter = TRUE)
 
-predictors <- c("date_day",
-                "date_month",
-                "date_hour",
-                "prop_location_score2",
-                "srch_destination_id",
-                "price_usd",
-                "srch_booking_window",
-                "prop_log_historical_price",
+predictors <- c(#"X",
+                #"srch_id",
+                #"date_time",
                 "site_id",
+                #"visitor_location_country_id",
+                "visitor_hist_starrating",
+                "visitor_hist_adr_usd",
+                #"prop_country_id",
+                #"prop_id",
+                "prop_starrating",
+                "prop_review_score",
+                "prop_brand_bool",
+                "prop_location_score1",
+                "prop_location_score2",
+                "prop_log_historical_price",
+                #"position",
+                "price_usd",
+                "promotion_flag",
+                #"srch_destination_id",
+                "srch_length_of_stay",
+                "srch_booking_window",
+                "srch_adults_count",
+                "srch_children_count",
+                "srch_room_count",
+                "srch_saturday_night_bool",
+                "srch_query_affinity_score",
+                "orig_destination_distance",
+                "random_bool",
                 "comp1_rate",
                 "comp1_inv",
                 "comp1_rate_percent_diff",
@@ -95,13 +110,34 @@ predictors <- c("date_day",
                 "comp7_rate_percent_diff",
                 "comp8_rate",
                 "comp8_inv",
-                "comp8_rate_percent_diff")
+                "comp8_rate_percent_diff",
+                "click_bool",
+                "gross_bookings_usd",
+                "booking_bool",
+                "date_day",
+                "date_month",
+                "date_hour",
+                "children_flag",
+                "price_diff_usd",
+                "star_rating_diff",
+                "prop_star_rating_monotonic",
+                "window_count",
+                "adult_child_count",
+                "ump",
+                "fee_per_person",
+                "total_fee",
+                "score2ma",
+                "score1d2")
 outcomeName <- 'booking_bool'
 
-model_rf<-train(trainSet[,predictors],trainSet[,outcomeName],method='rf',trControl=fitControl,tuneLength=3)
+#Training the Logistic regression model
+model_lr<-train(trainSet[,predictors],trainSet[,outcomeName],method='rf',trControl=fitControl,tuneLength=3)
 
-importance <- varImp(model_rf, scale=FALSE)
+importance <- varImp(model_knn, scale=FALSE)
 plot(importance)
 
-testSet$pred_rf<-predict(object = model_rf,testSet[,predictors])
-confusionMatrix(testSet$booking_bool,testSet$pred_rf)
+#Predicting using knn model
+testSet$pred_knn<-predict(object = model_knn,testSet[,predictors])
+
+#Checking the accuracy of the random forest model
+confusionMatrix(testSet$Loan_Status,testSet$pred_knn)
