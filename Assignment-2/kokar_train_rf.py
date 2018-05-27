fe093c2f5368af5c8ae09597dc72c2d54e69880f
izakdekom <izakdekom@gmail.com>
2018-05-27 17:12:21 +0200

    Removed gross_bookings_usd from predictors, silenced warnings, saves test kappa to txt file

--- Assignment-2/kokar_train_rf.py
+++ Assignment-2/kokar_train_rf.py
@@ -4,16 +4,18 @@ from sklearn.datasets import load_iris
 # Load scikit's random forest classifier library
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 
 # Load pandas
 import pandas as pd
+pd.options.mode.chained_assignment = None
 
 # Load numpy
 import numpy as np
 
 # Set random seed
 np.random.seed(0)
-
 
 # Create a dataframe with the four feature variables
 df = pd.read_csv("feature_eng_result.csv")
@@ -22,7 +24,7 @@ df = pd.read_csv("feature_eng_result.csv")
 train, test = train_test_split(df, test_size=0.25)
 
 # get features
-features = test.drop(['click_bool','position','Unnamed: 0','booking_bool','date_time','srch_id'], axis=1).columns
+features = test.drop(['click_bool','position','Unnamed: 0','booking_bool','date_time','srch_id','gross_bookings_usd'], axis=1).columns
 
 # Set click_bool as factor to be predicted
 y = pd.factorize(train['click_bool'])[0]
@@ -35,17 +37,15 @@ clf = RandomForestClassifier(n_estimators=100, n_jobs=2, random_state=0, verbose
 clf.fit(train[features], y)
 
 # Create predicted_clicks column for test dataset
-test["predicted_click"] = clf.predict(test[features])
+test["prediction_click"] = clf.predict(test[features])
 
 # Create predicted_clicks column for train dataset
-train["predicted_click"] = clf.predict(train[features])
-
-
+train["prediction_click"] = clf.predict(train[features])
 
 # AFTER PREDICTING  CLICK_BOOL, NOW WE CAN TRY TO PREDICT BOOKING_BOOL
 
 # get features (click_bool removed because we use predicted_clicks now)
-features = test.drop(['click_bool','position','Unnamed: 0','booking_bool','date_time','srch_id'], axis=1).columns
+features = test.drop(['click_bool','position','Unnamed: 0','booking_bool','date_time','srch_id','gross_bookings_usd'], axis=1).columns
 
 # Set click_bool as factor to be predicted
 y = pd.factorize(train['booking_bool'])[0]
@@ -58,8 +58,12 @@ clf = RandomForestClassifier(n_estimators=100, n_jobs=2, random_state=0, verbose
 clf.fit(train[features], y)
 
 # Create predicted_clicks column for test dataset
-test["predicted_booking"] = clf.predict(test[features])
+test["prediction_book"] = clf.predict(test[features])
 
+print("Kappa : %.4g" % metrics.cohen_kappa_score(test["booking_bool"].values, test["prediction_book"]))
 
+file = open('rf_book_kappa.txt', 'w')
+file.write(str(metrics.cohen_kappa_score(test["booking_bool"].values, test["prediction_book"])))
+file.close()
 
 test.to_csv("python_rf_result.csv")
